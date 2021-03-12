ba7f8135d4accd296b4c256a9e5189e60b6f5d20
Qaizx <kaothom@gmail.com>
2021-03-12 15:36:15 +0700

    finish

--- lab22_3.cpp
+++ lab22_3.cpp
@@ -1,8 +1,26 @@
 #include<iostream>
+#include<Set>
+#include<string>
 
 using namespace std;
 
+int count(int *y, unsigned int x){
+	set<int> mySet;
+
+	for(unsigned int i = 0; i < x; i++){
+	mySet.insert(y[i]);
+	}
+
+	int result;
+
+	result = mySet.size();
+
+	return result;
+
+}
 //Write function count() here.
+
+
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
